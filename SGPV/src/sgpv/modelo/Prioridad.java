916d4425ffb2d6d54b559668d49f8e96e040a5cb
kwjm7 <kwjm7@10.108.254.44>
2026-09-01 13:10:13 -0400

    feat(parte2): agregar clase enum Prioridad para eventos

---
+++ SGPV/src/sgpv/modelo/Prioridad.java
@@ -0,0 +1,12 @@
+package sgpv.modelo;
+
+/**
+ * representacion de las prioridad para los eventos
+ * @author Benja
+ */
+public enum Prioridad{
+    BAJA,
+    NORMAL,
+    ALTA,
+    CATASTROFE
+}
